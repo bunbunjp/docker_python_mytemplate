1684d13db581db47f94850b1658b850126eff3e7
Genki Kokubun <genki.kokubun@gmail.com>
2017-09-25 18:09:58 +0900

    kick run server.

--- environment/run.sh
+++ environment/run.sh
@@ -56,4 +56,4 @@ do
     VOLUME_OPT="${VOLUME_OPT} -v ${BASEDIR}/${D}:${PROJECT_DIR}/${D}"
 done
 
-docker run -it -p :8000 ${VOLUME_OPT} ${PROJECT_NAME} sh
+docker run -it -i -p 8000:8000 ${VOLUME_OPT} ${PROJECT_NAME} python ${PROJECT_NAME}/app/manage.py runserver 0.0.0.0:8000
